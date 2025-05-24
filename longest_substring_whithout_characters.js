8f3ccbf91014492f899613947e509c7f87e13b30
PratamarizkyPhe <pratamarizky249b@gmail.com>
2025-05-24 10:05:22 +0700

    longest_substring_whitout_char

--- longest_substring_whithout_characters.js
+++ longest_substring_whithout_characters.js
@@ -3,5 +3,22 @@
  * @return {number}
  */
 var lengthOfLongestSubstring = function(s) {
-    
+    let n = s.length;
+    let res = 0;
+
+    for (let i = 0; i < n; i++) {
+
+        let vis = new Array(26).fill(false);
+        for (let j = i; j < n; j++) {
+
+            if (vis[s.charCodeAt(j) - 'a'.charCodeAt(0)] === true)              
+                break;
+
+            else {
+                res = Math.max(res, j - i + 1);
+                vis[s.charCodeAt(j) - 'a'.charCodeAt(0)] = true;
+            }
+        }
+    }
+    return res;
 };
